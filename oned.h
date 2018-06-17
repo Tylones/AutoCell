d6cc3ac369db7d4fdc56ba350612f9f9d70eae9d
Unknown <etiennereitz@gmail.com>
2018-06-17 16:15:37 +0200

    Comments sur les automates cellulaires et etats

--- oned.h
+++ oned.h
@@ -12,13 +12,29 @@ public:
 
 
 	OneD(int width=100, int nbEtats=2,int r=126,int nb_neighborhood=3, QString name="");
-
+	/*!
+	 * \brief Cette fonction permet de changer l'état d'une cellule
+	 * \param point : QPoint récupéré lors du clic de l'utilisateur sur la render Area
+	 */
     void changeCellState(const QPoint point);
+	/*!
+	 * \brief Cette fonction permet à l'automate de passer à l'état suivant
+	 */
     void nextState();
     QVector<int> getRules() const;
     void setRules(const QVector<int> &value);
+	/*!
+	 * \brief remplit le tableau de règles
+	 * \param n : numéro de règle
+	 */
     void setRules(const int n);
+	/*!
+	 * \brief remplit aléatoirement la matrice de l'automate
+	 */
 	void generateRandomly();
+	/*!
+	 * \brief Permet de repasser à l'état précédent, virtuelle car la méthode varie en fonction de la dimension de l'automate
+	 */
 	void previous();
 	int getRule() const;
 protected:
